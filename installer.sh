f9ecb2bc3d9a323ebd3d27bc99910ebc7a4428d4
daflecardoso <daflesantos@gmail.com>
2022-03-03 16:20:28 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,14 +1,14 @@
 cd ~/
 DIR="gcli"
 if [ -d "$DIR" ]; then
-  sudo rm -R gcli
+  sudo rm -R .gcli
   echo "😳 Removing dir ${DIR}..."
 fi
 echo "😜 Clone project"
-git clone https://github.com/daflecardoso/gcli.git
+git clone https://github.com/daflecardoso/gcli.git .gcli
 echo "🤩 Giving permission"
-sudo chmod 777 gcli
-cd gcli
+sudo chmod 777 .gcli
+cd .gcli
 echo "🧐 Installing dependencies"
 npm install
 npm install -g
